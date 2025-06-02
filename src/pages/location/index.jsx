d5a529b6526c1a0a338192c42cb8013d37274958
mdfaiz9 <mdfaizalam827@gmail.com>
2025-06-02 10:28:52 +0530

    feat: update package.json and package-lock.json with new dependencies; implement DataTable component for location page

--- src/pages/location/index.jsx
+++ src/pages/location/index.jsx
@@ -1,38 +1,15 @@
-import React from 'react'
-import AddLocation from './layouts/AddLocation'
-import DataTable from 'react-data-table-component'
+import AddLocation from "./layouts/AddLocation";
+import DataTable from "../../components/layouts/data-table-components";
 
 const Location = () => {
-    const columns = [
-        {
-            name : '#',
-            selector: row => row.id,
-            sortable: true,
-        },
-        {
-            name: 'Location Name',
-            selector: row => row.name,
-            sortable: true,
-        },
-        {
-            name: 'Actions',
-            cell: row => <button className='bg-blue-500 text-white px-4 py-2 rounded'>Edit</button>,
-        },
-    ];
-    const data = [
-        { id: 1, name: 'Location A' },
-        { id: 2, name: 'Location B' },
-        { id: 3, name: 'Location C' },
-    ];
   return (
-    <div className='border-2 border-amber-300'>
-      <AddLocation/>
-      <div className='text-white'>
-        table
-        <DataTable columns={columns} data={data} fixedHeader pagination />
+    <div className="border-2 border-amber-300">
+      <AddLocation />
+      <div className="text-white">
+        <DataTable />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Location
+export default Location;
---
+++ src/pages/location/layouts/Data-table.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const dataTable = () => {
+  return <div>data-table</div>;
+};
+
+export default dataTable;
